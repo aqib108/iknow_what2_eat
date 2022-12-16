49fbcf5a06df30a96ca2e6035296d493a491236f
izain15 <96815774+izain15@users.noreply.github.com>
2022-12-16 20:56:50 +0500

    categoryList

---
+++ app/Models/Api/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Api;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name_en',
+        'name_ar',
+        'icon',
+        'custom_icon',
+        'status',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\HomeController;
@@ -40,5 +41,6 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::get('errorReportList',[ErrorGenerateController::class,'errorReportList']);
     Route::post('reportingError',[ErrorGenerateController::class,'errorReport']);
     Route::get('topItemsList',[RestaurantController::class,'topItemsList']);
+    Route::get('categoryList',[CategoryController::class,'categoryList']);
 
 });
